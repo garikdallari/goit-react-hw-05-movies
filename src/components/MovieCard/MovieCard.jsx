11fe9c66a4e3d3fdc9c5426389ad3e8f94bbf799
garikdallari <garikdallari@gmail.com>
2021-09-26 09:11:43 +0300

    add styles

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -3,27 +3,28 @@ import styles from "./MovieCard.module.css";
 
 function MovieCard({ movie }) {
   return (
-    <>
+    <div className={styles.thumb}>
       <img
         className={styles.img}
         src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
         alt=""
       />
-      <div>
+
+      <div className={styles.info}>
         <h2>
           {movie.title || movie.name} ({movie.release_date.slice(0, 4)})
         </h2>
         <p>User score : {movie.vote_average * 10}%</p>
         <b>Overview</b>
         <p>{movie.overview}</p>
-        <b>Genres</b>
+        <b>Genres:</b>
         <ul>
           {movie.genres.map((genre) => (
             <li key={genre.name}>{genre.name}</li>
           ))}
         </ul>
       </div>
-    </>
+    </div>
   );
 }
 
